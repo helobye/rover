6b3f5465b401e5e5356b345bc8cfaa455e5b8abd
Christian Graves <helo.bye@gmail.com>
2013-08-05 04:43:33 +0000

    Update rover.py

--- rover.py
+++ rover.py
@@ -22,72 +22,79 @@ def limit(n):
 	else:
 		return n
 
+while True:
 
-pygame.init()
-j = pygame.joystick.Joystick(0)
-j.init()
- 
-print 'Initialized Joystick : %s' % j.get_name()
+	pygame.init()
+	j = pygame.joystick.Joystick(0)
+	j.init()
+	print 'Initialized Joystick : %s' % j.get_name()
 		
-pygame.event.pump()
+	pygame.event.pump()
 
-if j.get_button(0): # Left
-	if DServo0Cur < DServo0[1]:
-		DServo0Cur = DServo0Cur + 100
-		print DServo0Cur
-if j.get_button(2): # Right
-	if DServo0Cur > DServo0[0]:
-		DServo0Cur = DServo0Cur - 100
-		print DServo0Cur
-if j.get_button(1): # Down
-	if DServo1Cur < DServo1[1]:
-		DServo1Cur = DServo1Cur + 100
-		print DServo1Cur
-if j.get_button(3): # Up
-	if DServo1Cur > DServo1[0]:
-		DServo1Cur = DServo1Cur - 100
-		print DServo1Cur
-if j.get_button(9): # Start. Reset to Default positions
-	DServo0Cur = DServo0[2]
-	DServo1Cur = DServo1[2]
+	if j.get_button(0): # Left
+		if DServo0Cur < DServo0[1]:
+			DServo0Cur = DServo0Cur + 100
+			
+	if j.get_button(2): # Right
+		if DServo0Cur > DServo0[0]:
+			DServo0Cur = DServo0Cur - 100
+			
+	if j.get_button(1): # Down
+		if DServo1Cur < DServo1[1]:
+			DServo1Cur = DServo1Cur + 100
 
-x = j.get_axis(0)
-y = j.get_axis(1)
-z = j.get_axis(2)
+	if j.get_button(3): # Up
+		if DServo1Cur > DServo1[0]:
+			DServo1Cur = DServo1Cur - 100
 
-left  = y - z
-right = y + z
+	if j.get_button(9): # Start. Reset to Default positions
+		DServo0Cur = DServo0[2]
+		DServo1Cur = DServo1[2]
+
+	# Poll Joystick for XYZ cordinates
+	x = j.get_axis(0)
+	y = j.get_axis(1)
+	z = j.get_axis(2)
+
+	# Define Tank Style controls
+	left  = y - z
+	right = y + z
         
-maxi = max(left, right)
+        # Make sure left, right values do not exceed maximum
+	maxi = max(left, right)
         
-if maxi > 1:
-	left = left/maxi
-	right = right/maxi
+	if maxi > 1:
+		left = left/maxi
+		right = right/maxi
     
-left = limit(left)
-right = limit(right)
+	left = limit(left)
+	right = limit(right)
 	
-move_y = int(round(left * 250, 0))
-if (move_y < 0):
-	drive_y = 2
-#elif (move_y == 0):
-#	drive_y = 1
-else:
-	drive_y = 0
-pwm_y = abs(move_y)
+	# Y axis (Forward/Reverse)
+	move_y = int(round(left * 250, 0))
+	if (move_y < 0):
+		drive_y = 2
+	#elif (move_y == 0):
+	#	drive_y = 1
+	else:
+		drive_y = 0
+	pwm_y = abs(move_y)
 
-move_z = int(round(right * 250, 0))
-if (move_z < 0):
-	drive_z = 2
-#elif (move_z == 0):
-#	drive_z = 1
-else:
-	drive_z = 0
-pwm_z = abs(move_z)
+	# Z Axis (Left/Right)
+	move_z = int(round(right * 250, 0))
+	if (move_z < 0):
+		drive_z = 2
+	#elif (move_z == 0):
+	#	drive_z = 1
+	else:
+		drive_z = 0
+	pwm_z = abs(move_z)
 	
-print(pwm_y,drive_y,pwm_z,drive_z)
-try:
-	bus.write_i2c_block_data(device,1,[drive_y, drive_z, pwm_y,pwm_z,DServo0Cur/10,DServo1Cur/10])
-except IOError, err:
-	print "Lost I2C"
-time.sleep(0.1)
+	print(pwm_y,drive_y,pwm_z,drive_z)
+	try:
+		bus.write_i2c_block_data(device,1,[drive_y, drive_z, pwm_y,pwm_z,DServo0Cur/10,DServo1Cur/10])
+	except IOError, err:
+		print "Lost I2C"
+		
+	# Sleep 100ms
+	time.sleep(0.1)
