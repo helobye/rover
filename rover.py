bdd8c7dbf25c3121316257e62466e95b347f26cb
Christian Graves <helo.bye@gmail.com>
2013-08-05 04:49:26 +0000

    Update rover.py

--- rover.py
+++ rover.py
@@ -2,17 +2,21 @@ import pygame
 import time
 import smbus
 
-# RPi rev 1 = SMBus(0)
-# RPi rev 2 = SMBus(1)
-bus = smbus.SMBus(1)
+bus = smbus.SMBus(1) # RPi rev 2 = SMBus(1)
 
-device = 0x04
+device = 0x04 # Arduino i2c device address
 
 # Servos. Min, Max, Default
 DServo0 = [700,2200,1500] # DServo0 L/R: Left:700, Right: 2200 Center:1500 
 DServo1 = [700,2300,2300] # DServo1 TIlt: Backard: 700, Forward: 2300, Max: 2300
 DServo0Cur = DServo0[2]
 DServo1Cur = DServo1[2]
+
+pygame.init() # Call PyGame
+j = pygame.joystick.Joystick(0) # j = Joystick 0
+j.init() # Initialize Joystick 0
+pygame.event.pump()
+print 'Initialized Joystick : %s' % j.get_name()
 
 def limit(n):
 	if n > 1:
@@ -23,14 +27,7 @@ def limit(n):
 		return n
 
 while True:
-
-	pygame.init()
-	j = pygame.joystick.Joystick(0)
-	j.init()
-	print 'Initialized Joystick : %s' % j.get_name()
 		
-	pygame.event.pump()
-
 	if j.get_button(0): # Left
 		if DServo0Cur < DServo0[1]:
 			DServo0Cur = DServo0Cur + 100
@@ -74,8 +71,6 @@ while True:
 	move_y = int(round(left * 250, 0))
 	if (move_y < 0):
 		drive_y = 2
-	#elif (move_y == 0):
-	#	drive_y = 1
 	else:
 		drive_y = 0
 	pwm_y = abs(move_y)
@@ -84,8 +79,6 @@ while True:
 	move_z = int(round(right * 250, 0))
 	if (move_z < 0):
 		drive_z = 2
-	#elif (move_z == 0):
-	#	drive_z = 1
 	else:
 		drive_z = 0
 	pwm_z = abs(move_z)
