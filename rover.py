cade27c3d86e692d8c0c4f1eb93bcdb9470e04ce
helobye <helo.bye@gmail.com>
2013-08-07 05:04:30 +0000

    Updated DATA Socket

--- rover.py
+++ rover.py
@@ -1,18 +1,20 @@
 import smbus  # Used for i2c
 import socket # Used for client communications
 #import sys
-#import select
+import select
 
-address = ('192.168.1.10', 6005)
-server_socket = socket(AF_INET, SOCK_DGRAM)
-server_socket.bind(address)
+UDP_IP = "192.168.1.10" # UDP Server
+UDP_PORT = 6005		# UDP Port
+sock = socket.socket(socket.AF_INET, # Internet
+					socket.SOCK_DGRAM) # UDP
+sock.bind((UDP_IP, UDP_PORT))
 
 bus = smbus.SMBus(1) # RPi rev 2 = SMBus(1)
 device = 0x04 # Arduino i2c device address
 
 while True:
         print "Listening"
-        recv_data, addr = server_socket.recvfrom(2048)
+        recv_data, addr = sock.recvfrom(2048)
         #if recv_data == "Request 1" :
         #       print "Received request 1"
         #       server_socket.sendto("Response 1", addr)
@@ -31,6 +33,6 @@ while True:
 
         print r_drive, l_drive, right, left, DServo0Cur, DServo1Cur
         try:
-                bus.write_i2c_block_data(device, 1, [r_drive, l_drive, right, l$
+                bus.write_i2c_block_data(device, 1, [r_drive, l_drive, right, left, DServo0Cur/10, DServo1Cur/10])
         except IOError, err:
                 print "Lost I2C"
