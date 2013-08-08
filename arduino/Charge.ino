4cfb3cb9572d1028849577fce7a9db095efab7ed
Christian Graves <helo.bye@gmail.com>
2013-08-08 05:57:39 +0000

    Update Charge.ino

--- arduino/Charge.ino
+++ arduino/Charge.ino
@@ -1,1 +1,15 @@
-#
+
+void Charge()
+{
+  if(volts>peakvolt) 
+  {
+    ctime=millis();                                           // reset safety timer if voltage increases
+    peakvolt=volts;                                           // update peak volt with new highest battery voltage
+  }
+  
+  if(millis()-ctime>safetime || volts<peakvolt-4)
+  {
+    cmode=0;                                                  // charge cycle complete 
+    peakvolt=0;                                               // reset peakvolt
+  } 
+}
