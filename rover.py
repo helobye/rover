a2cf779bbe9e913ca1d226e51e93db6f599c8f4d
Christian Graves <helo.bye@gmail.com>
2013-08-15 17:19:04 -0700

    Update rover.py

--- rover.py
+++ rover.py
@@ -1,18 +1,30 @@
 import smbus  # Used for i2c
 import socket # Used for client communications
+import piface.pfio as pfio # Used for relays/switches
 
+# Initialize UDP connection
 UDP_IP = "192.168.1.10" # UDP Server
 UDP_PORT = 6005		# UDP Port
 sock = socket.socket(socket.AF_INET, # Internet
 		socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
 
+# Initialize i2c
 bus = smbus.SMBus(1) # RPi rev 2 = SMBus(1)
 device = 0x04 # Arduino i2c device address
 
+# Initialize Piface IO
+pfio.init()
+r0 = pfio.Relay(0)
+r0State = 0
+r0CurState = 0
+r1 = pfio.Relay(1)
+r1State = 0
+r1CurState = 0
+
 while True:
         print "Listening"
-        recv_data, addr = sock.recvfrom(2048)
+        recv_data, addr = sock.recvfrom(1024)
         #if recv_data == "Request 1" :
         #       print "Received request 1"
         #       server_socket.sendto("Response 1", addr)
@@ -28,8 +40,27 @@ while True:
         left = recv_data[3]
         DServo0Cur = recv_data[4]
         DServo1Cur = recv_data[5]
+        r0state = recv_data[6]
+        r1state = recv_data[7]
 
-        print r_drive, l_drive, right, left, DServo0Cur, DServo1Cur
+        print r_drive, l_drive, right, left, DServo0Cur, DServo1Cur, r0state, r1state
+        
+	if r0State != r0CurState:
+		if r0State == 0:
+        		r0.turn_on()
+        		r0CurState = 1
+        	else
+        		r0.turn_off()
+        		r0CurState = 0
+        
+        if r1State != r1CurState:
+        	if r1State == 0:
+        		r1.turn_on()
+        		r1CurState = 1
+        	else
+        		r1.turn_off()
+        		r1CurState = 0
+        
         try:
                 bus.write_i2c_block_data(device, 1, [r_drive, l_drive, right, left, DServo0Cur/10, DServo1Cur/10])
         except IOError, err:
